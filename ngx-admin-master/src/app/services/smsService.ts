4397f4a72b355b168c328f4cbdaddd2202ee543c
Mateusz Chodkowski <chotkos@gmail.com>
2017-11-18 19:38:10 +0100

    smsService

---
+++ ngx-admin-master/src/app/services/smsService.ts
@@ -0,0 +1,46 @@
+import {NgModule} from '@angular/core'; 
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+export default class SMSService {
+
+    url:string = 'http://52.210.40.66:5000/sendsms';
+    constructor(private http: HttpClient){
+            
+    }
+
+    sendVerificationCode(phoneNumber:string, verificationNumber:string){
+
+        const req = this.http.post(this.url, {
+            code: verificationNumber,
+            phone_num: phoneNumber, 
+          })
+            .subscribe(
+              res => {
+                debugger;
+                console.log(res);
+              },
+              err => {
+                console.log("Error occured");
+              }
+            );
+    }
+
+    sendNotificationCode(phoneNumber:string, personName:string){
+        
+                const req = this.http.post(this.url, {
+                    name: personName,
+                    phone_num: phoneNumber, 
+                  })
+                    .subscribe(
+                      res => {
+                        debugger;
+                        console.log(res);
+                      },
+                      err => {
+                        console.log("Error occured");
+                      }
+                    );
+            }
+
+}

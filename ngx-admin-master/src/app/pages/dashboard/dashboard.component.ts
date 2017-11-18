8a2b8610d2025ea40b0188f368ce23fa5417a8bb
Rafal Margas <rafal.margas@gmail.com>
2017-11-18 22:38:01 +0100

    temp commit

--- ngx-admin-master/src/app/pages/dashboard/dashboard.component.ts
+++ ngx-admin-master/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MockData } from '../../model/MockData';
+import {Group} from "../../model/Group";
 
 @Component({
   selector: 'ngx-dashboard',
@@ -6,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
+  groups: Array<string> = new MockData().GetGroupsMock();
+  imageUrl: Array<string> = ["assets/images/alan.png",
+    "assets/images/alan.png",
+    "assets/images/alan.png",
+    "assets/images/alan.png",
+    "assets/images/alan.png",
+    "assets/images/alan.png"];
+  groupsObject: Array<Group> = [];
   ngOnInit(): void {
 
     setTimeout(function(){
       $('.menu-title').first().click();
     },500);
+
+    for (let i = 0 ; i < this.groups.length; i++) {
+      this.groupsObject.push(new Group(this.groups[i], this.imageUrl[i]))
+    }
   }
-
-
 }

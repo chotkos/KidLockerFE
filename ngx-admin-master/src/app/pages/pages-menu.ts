01a2ca6b5c5b8564624b11224a5c20914c8b4098
martako <martako>
2017-11-18 13:49:24 +0100

    adjusting views

--- ngx-admin-master/src/app/pages/pages-menu.ts
+++ ngx-admin-master/src/app/pages/pages-menu.ts
@@ -2,13 +2,7 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
-    icon: 'nb-home',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'FEATURES',
+    title: 'GRUPY',
     group: true,
   },
   {

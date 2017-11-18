c3914bbab310a9169b3f4082a4b1f1bdd5823341
Mateusz Chodkowski <chotkos@gmail.com>
2017-11-18 23:21:30 +0100

    added modal

--- ngx-admin-master/src/app/pages/ui-features/modals/modal/modal.component.ts
+++ ngx-admin-master/src/app/pages/ui-features/modals/modal/modal.component.ts
@@ -5,25 +5,22 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   selector: 'ngx-modal',
   template: `
     <div class="modal-header">
-      <span>{{ modalHeader }}</span>
+      <span>Kod weryfikacyjny</span>
       <button class="close" aria-label="Close" (click)="closeModal()">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
-    <div class="modal-body">
-      {{ modalContent }}
-    </div>
-    <div class="modal-footer">
-      <button class="btn btn-md btn-primary" (click)="closeModal()">Save changes</button>
-    </div>
+    <div class="modal-body" style='text-align:center'>
+      <div class='btn btn-primary btn-demo btn-lg' style='font-size:70px'>{{ modalHeader }}</div>
+      <hr/>
+      <div>{{ modalContent }}</div>
+    </div> 
   `,
 })
 export class ModalComponent {
 
   modalHeader: string;
-  modalContent = `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.`;
+  modalContent = `Wydaj dziecko tylko osobie, która jest w stanie powtórzyć ten kod.`;
 
   constructor(private activeModal: NgbActiveModal) { }
 
